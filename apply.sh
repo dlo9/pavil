420b9fd557a46edb546ba650e2b7868ab2a83218
David Orchard <if_coding@fastmail.com>
2021-02-08 00:16:57 -0800

    Fix curcular linking bug

--- apply.sh
+++ apply.sh
@@ -13,12 +13,9 @@ TARGET_SLS_DIR="/srv/salt"
 
 echo "Installing salt config..."
 cp -a "$LOCAL_CONFIG_DIR/"* "$TARGET_CONFIG_DIR"
-#cp -a config/* /etc/salt/
 chown -R root:root "$TARGET_CONFIG_DIR"
-#chown -R root:root /etc/salt
 # TODO: copy and chown instead?
-#ln -sf ./salt /srv/salt
-ln -sf "$LOCAL_SLS_DIR" "$TARGET_SLS_DIR" # /srv/salt
+ln -sfT "$LOCAL_SLS_DIR" "$TARGET_SLS_DIR" # /srv/salt
 
 echo "Running salt..."
 salt-call --local state.apply
